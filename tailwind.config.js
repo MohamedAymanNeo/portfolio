8ee9a870dbe4626444d1b50ddf79d5af03b9fe78
MohamedAymanNeo <az.mohamedayman12@gmail.com>
2022-05-07 23:16:17 +0200

    finish home Component

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,21 +31,32 @@ module.exports = {
         '37%':'37%',
       },
       lineHeight: {
+        '0':'0',
         '12': '3rem',
         '14': '4rem'
+      },
+      borderWidth: {
+        '1':'1px'
       },
       borderRadius: {
         '4xl':'2rem',
         '5xl':'2.2rem',
+        '50%':'50%'
       },
       width: {
         'cst':'3.2rem',
         '600px':'38rem',
+        '55px':'55px',
       },
       height: {
+        '1px':'1px',
         'cst':'3.2rem',
-        '2full': '200%'
+        '2full': '200%',
+        '55px':'55px',
       },
+      padding: {
+        '18': '4.375rem'
+      }
     },
   },
   plugins: [],
