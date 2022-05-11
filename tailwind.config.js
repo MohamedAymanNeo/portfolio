81ad26d5d3bfd99bdd48573d2aeda5cfcffd8993
MohamedAymanNeo <az.mohamedayman12@gmail.com>
2022-05-12 01:00:37 +0200

    fix class issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,8 @@ module.exports = {
   content: ['./src/**/*.{html,ts}'],
   theme: {
     container: {
-      // you can configure the container to be centered
       center: true,
-
-      // or have default horizontal padding
       padding: '1rem',
-
-      // default breakpoints but with 40px removed
       screens: {
         sm: '600px',
         md: '728px',
