96b8ab2d263d67d37a8a66f10c6eea02c36f6b49
MohamedAymanNeo <az.mohamedayman12@gmail.com>
2022-05-18 22:38:35 +0200

    push updates

--- src/app/layout/components/exp-education/exp-education.component.ts
+++ src/app/layout/components/exp-education/exp-education.component.ts
@@ -9,8 +9,13 @@ export class ExpEducationComponent implements OnInit {
 
   eduExpArray = [
     {name: 'experience', icon:'fa fa-briefcase' ,experienceContent: [
-      {time: 'Feb.2020- present', position: 'Mid-Senior Frontend Developer', company: 'qTech (alqemam)'},
-      {time: 'Jun.2019 - Feb.2020', position: 'Junior Frontend Developer', company: 'Delta Soft'},
+      {time: 'Feb.2020- present', position: 'Mid-Senior Frontend Developer', 
+      company: 'qTech (alqemam)',
+      content:'Work as a front developer since I came to the company where I developed my self and skills'
+      },
+      {time: 'Jun.2019 - Feb.2020', position: 'Junior Frontend Developer', 
+      company: 'Delta Soft', content:'Work as frontend developer on applications using ionic'
+      },
       
     ]},
     {name: 'education', icon:'fa fa-graduation-cap' ,educationContent: [
